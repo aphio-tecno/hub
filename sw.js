0113d222747ca5e72d9f0a7e5284180dbcfe5098
Tecnomotor <aphio@tecnomotordi.com.br>
2026-03-12 09:39:38 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,40 @@
-self.addEventListener('install', (e) => {
-  console.log('TM Hub: Service Worker Instalado');
+const CACHE_NAME = 'tm-hub-v1.2';
+
+// Ficheiros a serem guardados em cache (opcional, mas ajuda na velocidade)
+const urlsToCache = [
+  './',
+  './manifest.json?v=1.2'
+];
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request));
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('A apagar cache antiga:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
 });
